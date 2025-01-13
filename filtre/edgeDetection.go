b1f345adde43fafdc6b054af5a40bd97f0e3ea77
Marine <marine.collet@insa-lyon.fr>
2025-01-13 15:47:06 +0100

    Réorganisation arborescence

---
+++ filtre/edgeDetection.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"fmt"
+	"image/color"
+	"math"
+
+	"gonum.org/v1/gonum/mat"
+)
+
+// https://medium.com/@damithadayananda/image-processing-with-golang-8f20d2d243a2
+
+func Decoupe_image(N int, img [][]color.Color) []int {
+
+	/* Fonction qui découpe l'image en N bandes horizontales. N est passé en argument et pour
+	   une exécution optimale, il doit être égale au nombre de thread de notre ordi.
+	   Renvoie les coordonnées de chaque bande.
+	   Notre fonction edgeDetection utilise les coordonnées retournées par cette fonction
+	   L'image prise en argument doit être une slice de slices de color.Color et non pas de
+	   type image.Image
+	   liste_hauteurs renvoyée est de taille N+1
+	*/
+
+	pimg := img
+
+	hauteur_bande := len(pimg) / N
+	liste_hauteurs := make([]int, N+1) // // Créé un tableau dynamique, qui nous permet d'utiliser N
+
+	for i := 0; i <= N; i++ {
+		liste_hauteurs[i] = i * hauteur_bande
+	}
+
+	return liste_hauteurs
+}
+
+// pixels est équivalent à img, &pixels est l'adresse de l'image à traiter
+
+/*Ce processus utilise les filtres de Sobel pour détecter les contours dans une image en niveaux de gris, en
+calculant les gradients dans les directions horizontales et verticales, puis en combinant ces gradients pour
+déterminer la présence de contours. */
+
+func EdgeDetection(pixels *[][]color.Color, x_haut int, x_bas int) {
+
+	if pixels == nil || *pixels == nil {
+		fmt.Println("Erreur: pixels est nil ou non initialisé")
+		return
+	}
+	ppixels := *pixels
+
+	//make image grey scale
+	for x := x_haut; x <= x_bas; x++ { // On parcourt tous les pixels de limage
+		for y := 0; y < len(ppixels[0]); y++ {
+			r, g, b, a := ppixels[x][y].RGBA()                             // Pour chaque pixel, on récupère ses composantes rouge, verte, bleue et alpha
+			grey := 0.299*float64(r) + 0.587*float64(g) + 0.114*float64(b) // On calcule une valeur de gris, qui correspond à une pondération des composantes RGB pour refléter la perception humaine des couleurs.
+			c := uint8(grey + 0.5)
+			ppixels[x][y] = color.RGBA{ // On crée un nouveau pixel avec les trois composantes (R, G, B) égales à cette valeur de gris, ce qui rend l'image en niveaux de gris.
+				R: c,
+				G: c,
+				B: c,
+				A: uint8(a),
+			}
+		}
+	}
+	// Deux matrices 3×3, kernelx et kernely, sont définies pour détecter les contours horizontaux et verticaux respectivement. Ces matrices sont utilisées pour appliquer des filtres de Sobel, un algorithme courant pour la détection de contours.
+	kernelx := mat.NewDense(3, 3, []float64{
+		1, 0, 1,
+		-2, 0, 2,
+		-1, 0, 1,
+	})
+	kernely := mat.NewDense(3, 3, []float64{
+		-1, -2, -1,
+		0, 0, 0,
+		1, 2, 1,
+	})
+	//create two dimensional array to store intensities of each pixels
+	intensity := make([][]int, len(ppixels))
+	for y := 0; y < len(intensity); y++ { // On parcourt l'image pour remplir ce tableau avec les valeurs de gris calculées.
+		intensity[y] = make([]int, len(ppixels[0]))
+	}
+	//calculate intensities : calcule et stocke les valeurs de gris dans un tableau séparé sans modifier l'image, en préparation pour l'application des filtres de Sobel.
+	for i := 0; i < len(ppixels); i++ {
+		for j := 0; j < len(ppixels[0]); j++ {
+			if i >= 0 && i < len(ppixels) && j >= 0 && j < len(ppixels[0]) {
+				colors := color.RGBAModel.Convert(ppixels[i][j]).(color.RGBA)
+				r := colors.R
+				g := colors.G
+				b := colors.B
+				v := int(float64(float64(0.299)*float64(r) + float64(0.587)*float64(g) + float64(0.114)*float64(b)))
+				intensity[i][j] = v
+			}
+		}
+	}
+	//create new image
+	newImage := make([][]color.Color, len(ppixels)) // Un tableau newImage est créé pour stocker l'image après l'application des filtres de Sobel.
+	for i := 0; i < len(newImage); i++ {
+		newImage[i] = make([]color.Color, len(ppixels[0]))
+	}
+
+	for x := 1; x < len(ppixels)-1; x++ { // On parcourt chaque pixel de l'image, sauf ceux sur les bords (pour éviter de sortir des limites lors de la convolution).
+		for y := 1; y < len(ppixels[0])-1; y++ {
+			var magx, magy int
+			for a := 0; a < 3; a++ { // Pour chaque pixel, on applique les noyaux kernelx et kernely pour calculer les gradients horizontal (magx) et vertical (magy).
+				for b := 0; b < 3; b++ {
+					xn := x + a - 1
+					yn := y + b - 1
+
+					// On vérifie que les indices soient dans les limites de l'image
+					if xn >= 0 && xn < len(ppixels) && yn >= 0 && yn < len(ppixels[0]) {
+						magx += intensity[xn][yn] * int(kernelx.At(a, b))
+						magy += intensity[xn][yn] * int(kernely.At(a, b))
+					}
+				}
+			}
+			p := int(math.Sqrt(float64(magx*magx + magy*magy))) // La magnitude du gradient est calculée en utilisant la racine carrée de la somme des carrés des gradients horizontal et vertical.
+			newImage[x][y] = color.RGBA{                        // Un nouveau pixel est créé avec la valeur de la magnitude comme composante RGB (valeur du contour détecté), et l'alpha est mis à zéro.
+				R: uint8(p),
+				G: uint8(p),
+				B: uint8(p),
+				A: 0,
+			}
+		}
+	}
+	*pixels = newImage // Le pointeur pixels est mis à jour pour pointer vers la nouvelle image contenant les contours détectés.
+}
---
+++ filtre/image_encoder_decoder.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/gif"
+	"image/jpeg"
+	"image/png"
+	"os"
+)
+
+func ImageToColorMatrix(img image.Image) [][]color.Color {
+	// Obtenir les dimensions de l'image
+	bounds := img.Bounds()
+	width, height := bounds.Max.X, bounds.Max.Y
+
+	// Créer une matrice 2D pour stocker les couleurs des pixels
+	colorMatrix := make([][]color.Color, height)
+	for y := 0; y < height; y++ {
+		colorMatrix[y] = make([]color.Color, width)
+		for x := 0; x < width; x++ {
+			// Extraire la couleur du pixel à la position (x, y)
+			colorMatrix[y][x] = img.At(x, y)
+		}
+	}
+
+	return colorMatrix
+}
+
+// MatrixToImage prend une matrice de couleurs ([][]color.Color) et la convertit en une image.Image
+func MatrixToImage(matrix [][]color.Color) image.Image {
+
+	if len(matrix) == 0 || len(matrix[0]) == 0 {
+		fmt.Println("Erreur: la matrice est vide ou mal initialisée")
+		return nil
+	}
+
+	// Obtenir les dimensions de la matrice
+	height := len(matrix)
+	width := len(matrix[0])
+
+	// Créer une nouvelle image avec la même taille que la matrice
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+
+	// Remplir l'image avec les couleurs de la matrice
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+
+			if matrix[y][x] == nil {
+				fmt.Printf("Erreur: pixel à [%d][%d] est nil, utilisation du noir par défaut\n", y, x)
+				img.Set(x, y, color.Black) // Utilise une couleur par défaut (ici le noir)
+				continue
+			}
+
+			// On prend la couleur de la matrice à la position (x, y)
+			c := matrix[y][x]
+			// On définit le pixel de l'image avec la couleur
+			img.Set(x, y, c)
+		}
+	}
+
+	return img
+}
+
+func DecodeImage(filename string) (image.Image, string, error) {
+	/* Cette fonction prend en argument une image dans un format jpg, png, ...
+	et renvoie cette image en type image.Image (qui est un type propre à go)
+	ainsi que le format de l'image décodée (jpeg, png,...) */
+	file, err1 := os.Open(filename)
+	if err1 != nil {
+		return nil, "", err1
+	}
+	defer file.Close()
+
+	img, format, err1 := image.Decode(file)
+	if err1 != nil {
+		return nil, "", err1
+	}
+
+	return img, format, nil
+}
+
+func EncodeImage(filename string, img image.Image, format string) error {
+	/* Cette fonction prend en argument une image de type image.Image
+	(qui est un type propre à go) et renvoie cette image dans un format jpg, png, ...
+	en fonction de la valeur de "format" passé en argument. */
+
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	switch format {
+	case "jpeg", "jpg":
+		return jpeg.Encode(file, img, nil)
+	case "png":
+		return png.Encode(file, img)
+	case "gif":
+		return gif.Encode(file, img, nil)
+	default:
+		return fmt.Errorf("unsupported format: %s", format)
+	}
+}
+
+/*	*********************************Pour décoder l'image***********************
+	img, format, err1 := decodeImage("path/to/image.jpg")
+	if err1 != nil {
+		fmt.Println("Error decoding image:", err1)
+		return
+	}
+	fmt.Printf("Decoded image format: %s\n", format)
+
+	*********************************Pour encoder l'image*****************
+	err := encodeImage("path/to/output.jpg", img, format)
+	if err != nil {
+		fmt.Println("Error encoding image:", err)
+		return
+	}
+
+	fmt.Println("Image saved successfully") */
