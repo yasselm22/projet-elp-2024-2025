49adba714dc16da603a77ef5ea9e41473109c135
bmuccini <muccini.bianca@gmail.com>
2025-01-24 13:59:23 +0100

    Ajout Main.elm et style.css

--- projet_elm/src/Main.elm
+++ projet_elm/src/Main.elm
@@ -1,1 +1,55 @@
 module Main exposing (..)
+
+import Browser
+import Html exposing (Html, div, input, text)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
+
+-- MODEL
+type alias Model =
+    { commands : String }
+
+init : Model
+init =
+    { commands = "" }
+
+
+-- UPDATE
+type Msg
+    = UpdateCommands String
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        UpdateCommands newCommands ->
+            { model | commands = newCommands }
+
+
+-- VIEW
+view : Model -> Html Msg
+view model =
+    div []
+        [ input 
+            [ placeholder "Entrez des commandes de tracé"
+            , onInput UpdateCommands
+            , ariaLabel "Champ de commandes"
+            ] 
+            []
+        , svg [ width "500", height "500", viewBox "0 0 500 500" ]
+            (renderCommands model.commands)
+        ]
+
+-- RENDER COMMANDS
+renderCommands : String -> List (Svg msg)
+renderCommands commands =
+    if commands /= "" then
+        [ circle [ cx "250", cy "250", r "50", fill "red" ] [] ]
+    else
+        []
+        
+
+-- MAIN
+main =
+    Browser.sandbox { init = init, update = update, view = view }
