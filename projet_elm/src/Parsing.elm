32c356c6ad86c4b7013dc2d1d73c2faf461a9e7f
bmuccini <muccini.bianca@gmail.com>
2025-01-28 00:23:14 +0100

    Code Parsing.elm

--- projet_elm/src/Parsing.elm
+++ projet_elm/src/Parsing.elm
@@ -1,1 +1,67 @@
 module Parsing exposing (..)
+
+import Parser exposing (Parser, (|.), (|=), succeed, symbol, int, float, spaces, lazy, sequence)
+
+type Command
+    = Forward Float
+    | Left Float
+    | Right Float
+    | Repeat Int (List Command)
+
+commandParser : Parser Command
+commandParser =
+    Parser.oneOf
+        [ repeatParser  -- Tester d'abord les commandes complexes
+        , forwardParser
+        , leftParser
+        , rightParser   
+        ]
+
+forwardParser : Parser Command
+forwardParser =
+    succeed Forward
+        |. symbol "Forward"
+        |. spaces
+        |= float
+
+leftParser : Parser Command
+leftParser =
+    succeed Left
+        |. symbol "Left"
+        |. spaces
+        |= float
+
+rightParser : Parser Command
+rightParser =
+    succeed Right
+        |. symbol "Right"
+        |. spaces
+        |= float
+
+repeatParser : Parser Command
+repeatParser =
+    succeed Repeat
+        |. symbol "Repeat"
+        |. spaces
+        |= int
+        |. spaces
+        |. symbol "["
+        |= lazy (\_ -> sequence { start = "", separator = ",", end = "]", spaces = spaces, item = commandParser, trailing = Parser.Forbidden }) -- Sans lazy, on aurait une récursion infinie car commandParser dépend de lui-même (pour les commandes imbriquées)
+        |. spaces
+        |. symbol "]"
+        |. spaces
+
+-- Pour parser une liste complète de commandes
+programParser : Parser (List Command)
+programParser =
+    sequence
+        { start = "["
+        , separator = ","
+        , end = "]"
+        , spaces = spaces
+        , item = commandParser
+        , trailing = Parser.Forbidden
+        }
+
+
+-- Faire un message d'erreur si input autre chose que les commandes données
