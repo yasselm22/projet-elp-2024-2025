816a7b4fcad56ade97ed741311e1588ac712cb36
bmuccini <muccini.bianca@gmail.com>
2025-01-28 01:03:43 +0100

    Modif 1 Main.elm

--- projet_elm/src/Main.elm
+++ projet_elm/src/Main.elm
@@ -1,55 +1,91 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, div, input, text)
-import Html.Attributes exposing (..)
+import Html exposing (Html, div, text, textarea, pre)
+import Html.Attributes exposing (style, value)
 import Html.Events exposing (onInput)
-import Svg exposing (..)
-import Svg.Attributes exposing (..)
+import Parsing exposing (Command, programParser)  -- Importez programParser depuis Parsing.elm
+import Parser
+--import Svg exposing (..)
+--import Svg.Attributes exposing (..)
 
 -- MODEL
 type alias Model =
-    { commands : String }
+    { input : String
+    , commands : Result String (List Command)  -- Stocke le résultat du parsing (succès/erreur)
+    }
 
 init : Model
 init =
-    { commands = "" }
-
+    { input = "Exemple : [Forward 100, Repeat 4 [Forward 50, Left 90], Forward 100]"  -- C'est meiux le champ vide au départ ?
+        , commands = Ok []
+        }
 
 -- UPDATE
 type Msg
-    = UpdateCommands String
+    = InputChanged String
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
     case msg of
-        UpdateCommands newCommands ->
-            { model | commands = newCommands }
+        InputChanged newInput ->
+            ( { model 
+                | input = newInput
+                , commands = 
+                    Parser.run programParser newInput
+                        |> Result.mapError (\_ -> "Erreur de syntaxe")
+              }
+            , Cmd.none
+            )
 
 
 -- VIEW
 view : Model -> Html Msg
 view model =
-    div []
-        [ input 
-            [ placeholder "Entrez des commandes de tracé"
-            , onInput UpdateCommands
-            , ariaLabel "Champ de commandes"
+    div [ style "padding" "20px" ]
+        [ -- Zone de saisie
+          textarea 
+            [ value model.input
+            , onInput InputChanged
+            , style "width" "400px"
+            , style "height" "150px"
+            , style "margin-bottom" "20px"
             ] 
             []
-        , svg [ width "500", height "500", viewBox "0 0 500 500" ]
-            (renderCommands model.commands)
+          
+          -- Affichage des commandes parsées (pour débogage)
+        , case model.commands of
+            Ok commands ->
+                div []
+                    [ text "Commandes parsées avec succès :"
+                    , pre [] [ text (Debug.toString commands) ]  -- Affiche la structure Elm
+                    ]
+
+            Err errorMsg ->
+                div [ style "color" "red" ] [ text errorMsg ]
         ]
 
 -- RENDER COMMANDS
-renderCommands : String -> List (Svg msg)
-renderCommands commands =
-    if commands /= "" then
-        [ circle [ cx "250", cy "250", r "50", fill "red" ] [] ]
-    else
-        []
+--renderCommands : String -> List (Svg msg)
+--renderCommands commands =
+    --if commands /= "" then
+        --[ circle [ cx "250", cy "250", r "50", fill "red" ] [] ]
+    --else
+        --[]
         
 
 -- MAIN
+main : Program () Model Msg
 main =
-    Browser.sandbox { init = init, update = update, view = view }
+    Browser.element
+        { init = \_ -> ( init, Cmd.none )
+        , update = update
+        , view = view
+        , subscriptions = \_ -> Sub.none
+        }
+--main =
+    --Browser.sandbox
+        --{ init = init
+        --, update = update
+        --, view = view
+        --}
